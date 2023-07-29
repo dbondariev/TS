4b97eed4ad0bd530e7580ff1f2910a9d99b7fa78
Dima Bondariev <dima.bondariev@gmail.com>
2023-07-29 15:35:07 +0300

    function types

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ function printResult(num) {
     console.log("Result:" + num);
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
